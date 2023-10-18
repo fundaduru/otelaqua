77f35da4249f8c27d48fc48723ac25ea5de87893
Funda <fundaduru0@gmail.com>
2023-10-18 15:04:58 +0300

    ss

--- proje/proje.js
+++ proje/proje.js
@@ -5,19 +5,33 @@ rezervasyon.onclick= function(){
     
     let r1=new Date(document.getElementById("r1").value);
     let r2=new Date(document.getElementById("r2").value);
+let r3=document.getElementById("r3").value;
+let r4=document.getElementById("r4").value;
 
-
-    if(r1>r2){
+   
+    if (r4==""  ){
         Swal.fire({
             
-            text: 'Çıkış tarihiniz giriş tarihinizden önce olamaz.',
+            text: 'Boş alan bırakmayınız.',
         
             confirmButtonText: 'Tamam'
-          });
+          }); 
     }
+
+
     if(r2>r1){
     localStorage.setItem("giriş tarihi",r1);
     localStorage.setItem("çıkış tarihi",r2);
+    localStorage.setItem("oda",r3);
+    localStorage.setItem("kişi sayısı",r4);
+    }
+    else{
+        Swal.fire({
+            
+            text: 'Tarihleri giriniz.Çıkış tarihiniz giriş tarihinizden önce olamaz.',
+        
+            confirmButtonText: 'Tamam'
+          }); 
     }
     
 

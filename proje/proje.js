a370515b57792c0ec1a2b51b8c496f025a172703
Funda <fundaduru0@gmail.com>
2023-10-18 17:09:14 +0300

    dd

--- proje/proje.js
+++ proje/proje.js
@@ -3,13 +3,15 @@
 rezervasyon.onclick= function(){
 
     
-    let r1=new Date(document.getElementById("r1").value);
-    let r2=new Date(document.getElementById("r2").value);
-let r3=document.getElementById("r3").value;
-let r4=document.getElementById("r4").value;
+let giris=new Date(document.getElementById("r1").value);
+let cikis=new Date(document.getElementById("r2").value);
+let oda=document.getElementById("r3").value;
+let kisi=document.getElementById("r4").value;
+let isim=document.getElementById("isim").value;
+let mail=document.getElementById("mail1").value;
 
    
-    if (r4==""  ){
+    if (r4=="" || isim=="" || mail==""){
         Swal.fire({
             
             text: 'Boş alan bırakmayınız.',
@@ -17,9 +19,23 @@ let r4=document.getElementById("r4").value;
             confirmButtonText: 'Tamam'
           }); 
     }
+    if (r1>r2){
+        Swal.fire({
+            
+            text: 'Çıkış tarihi giriş tarihinden önce olamaz.',
+        
+            confirmButtonText: 'Tamam'
+          }); 
+    
+
+    }
+
+    if (r2>r1){
+
+    }
 
 
-    if(r2>r1){
+    /*if(r2>r1){
     localStorage.setItem("giriş tarihi",r1);
     localStorage.setItem("çıkış tarihi",r2);
     localStorage.setItem("oda",r3);
@@ -41,7 +57,8 @@ let r4=document.getElementById("r4").value;
         
             confirmButtonText: 'Tamam'
           }); 
-    }
+    }*/
     
 
 }
+
